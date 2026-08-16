5cd5e28d846a2a6ebe863957619d819cf1cdc8c9
0mercansiskolu <omercansiskolu01@gmail.com>
2026-08-16 17:58:26 +0300

    Add Lacivert Sports Cloudstream module metadata

---
+++ cloudstream-lacivert/LacivertSports/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    language = "tr"
+    description = "Lacivert Sports için izinli canlı yayın kaynaklarını Cloudstream içinde listeleyen eklenti."
+    authors = listOf("0mercansiskolu")
+    status = 1
+    tvTypes = listOf("Live")
+}
